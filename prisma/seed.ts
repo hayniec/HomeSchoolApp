b21c22a1b24d8fc6cf876b645b245ecdc78dab41
Eric H <eric.haynie@gmail.com>
2026-02-21 17:53:22 -0600

    Add default administrator account with seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const email = 'admin@admin.com'
+    const password = await bcrypt.hash('admin123', 10)
+
+    const admin = await prisma.user.upsert({
+        where: { email },
+        update: {},
+        create: {
+            email,
+            name: 'Administrator',
+            password,
+        },
+    })
+
+    console.log({ admin })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
